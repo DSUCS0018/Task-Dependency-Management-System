b4ed366c82fd24ec06516e1744da72b6c01c26e7
Ankita Ishwar Bellad <ankitabellad88@gmail.com>
2026-01-21 14:40:59 +0530

    feat: expose dependencies in task api

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from .models import Task, TaskDependency
 
 class TaskSerializer(serializers.ModelSerializer):
+    dependencies = serializers.SerializerMethodField()
+
     class Meta:
         model = Task
         fields = '__all__'
+
+    def get_dependencies(self, obj):
+        return list(obj.dependencies.values_list('depends_on_id', flat=True))
 
 class TaskDependencySerializer(serializers.Serializer):
     depends_on_id = serializers.IntegerField()
